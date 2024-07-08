c4f0b19f99205ad4a48069232da01411c0452dd2
PhoenixStormJr <66498788+PhoenixStormJr@users.noreply.github.com>
2024-07-08 07:54:20 +0000

    Update SetTime.py

--- scripts/SetTime.py
+++ scripts/SetTime.py
@@ -1,52 +1,69 @@
 #ONLY CHANGE THIS VARIABLE:
-Add_Hours = 23
+zone = 8
 
 
 
 
 import os
-def GetTime(Format,Add_Hours=0):
+def GetTime(zone,format):
+  zoneSites = ['https://time.is/Baker_Island', 'https://time.is/Pago_Pago', 'https://time.is/Avarua', 'https://time.is/Atuona', 'https://time.is/Rikitea', 'https://time.is/Alaska', 'https://time.is/Los_Angeles', 'https://time.is/Albuquerque', 'https://time.is/Chicago', 'https://time.is/New_York', 'https://time.is/S%C3%A3o_Paulo', 'https://time.is/Newfoundland_and_Labrador', 'https://time.is/Saint_Pierre_and_Miquelon', 'https://time.is/Praia', 'https://time.is/Bobo-Dioulasso', 'https://time.is/Algiers', 'https://time.is/Durr%C3%ABs', 'https://time.is/Manama', 'https://time.is/Isfahan', 'https://time.is/Yerevan', 'https://time.is/Kabul', 'https://time.is/Almaty', 'https://time.is/Ahmedabad', 'https://time.is/Biratnagar', 'https://time.is/Chittagong', 'https://time.is/Naypyidaw', 'https://time.is/Phnom_Penh', 'https://time.is/Mandurah', 'https://time.is/Ambon_City', 'https://time.is/Adelaide', 'https://time.is/Australian_Capital_Territory', 'https://time.is/Palikir', 'https://time.is/Suva', 'https://time.is/Apia', 'https://time.is/Tabwakea_Village']
   import urllib.request
   #Get time from server:
   headers = {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
   }
-  mystr = urllib.request.urlopen(urllib.request.Request("https://time.is/New_York", headers=headers)).read().decode("utf8")
+  mystr = urllib.request.urlopen(urllib.request.Request(zoneSites[zone], headers=headers)).read().decode("utf8")
   location = mystr.find('<time id="clock">')
-  new_str = mystr[location:location+83]
-  time = new_str[17:25]
-  AMPM = new_str[81:83]
-  #Time is "time" and AMPM is "AMPM"
-  #Split time into hour minute second
-  timesplit = time.split(":")
-  hour = int(timesplit[0])
-  minute = timesplit[1]
-  second = timesplit[2]
-  #Now convert to military time
-  if(AMPM == "PM"):
-    hour = hour + 12
-  #Add hours
-  hour = hour + Add_Hours
-  if(hour > 24):
-    hour = hour - 24
-  if(Format==1):
-    if(hour < 10):
-      hour = "0" + str(hour)
-    time2= f"{hour}:{minute}:{second}"
-    return time2
-  elif(Format==2):
-    if(hour > 12):
-      hour = hour - 12
-      if(hour < 10):
-        hour = "0" + str(hour)
-      time2= f"{hour}:{minute}:{second}PM"
+  AMPM = mystr[location+81:location+83]
+  time_str = mystr[location+17:location+25]
+  if(format == 2):
+    time_split = time_str.split(':')
+    if(AMPM == 'PM'):
+      hour = str(int(time_split[0])+12)
     else:
-      if(hour < 10):
-        hour = "0" + str(hour)
-      time2= f"{hour}:{minute}:{second}AM"
-    return time2
+      hour = time_split[0]
+    minute = time_split[1]
+    second = time_split[2]
+    time=f"{hour}:{minute}:{second}"
+  elif(format == 1):
+    time = time_str+AMPM
+  location = mystr.find('title="Press for calendar">')
+  date_str = mystr[location+35:location+47]
+  date_str = date_str.replace(',','')
+  date_split = date_str.split(' ')
+  month = date_split[0]
+  day = date_split[1]
+  year = date_split[2]
+  if(month == 'January'):
+    month2 = '01'
+  if(month == 'February'):
+    month2 = '02'
+  if(month == 'March'):
+    month2 = '03'
+  if(month == 'April'):
+    month2 = '04'
+  if(month == 'May'):
+    month2 = '05'
+  if(month == 'June'):
+    month2 = '06'
+  if(month == 'July'):
+    month2 = '07'
+  if(month == 'August'):
+    month2 = '08'
+  if(month == 'September'):
+    month2 = '09'
+  if(month == 'October'):
+    month2 = '10'
+  if(month == 'November'):
+    month2 = '11'
+  if(month == 'December'):
+    month2 = '12'
+  date = f"{year}-{month2}-{day}"
+  return f"{date} {time}"
 
-time = GetTime(2,Add_Hours)
-print(f"\"time is: {time}\"")
-print(f'running command [ date +"%T%p" -s "{time}" ]')
-os.system(f'date +"%T%p" -s "{time}"')
+
+
+Date_And_Time=GetTime(zone,2)
+print(f"Date and Time is: \"{Date_And_Time}\"")
+print(f"running command: date -s '{Date_And_Time}'")
+os.system(f"date -s '{Date_And_Time}'")
